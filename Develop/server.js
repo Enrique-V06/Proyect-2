5b90383af449858a617e15d5b088c3eab4ffc8ea
David Mondragon <dragon18saiz@gmail.com>
2023-06-20 10:00:43 -0600

    created homepage.handlebars, added env.folder, modified server.js to work with express-handlebars

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
-
+const exphbs = require('express-handlebars');
 // model import:
 const Users = require('./model');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,9 +15,9 @@ app.set('view engine', 'handlebars');
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
-app.use(routes);
 // sync sequelize models to the database, then turn on the server
 sequelize
   .sync()
