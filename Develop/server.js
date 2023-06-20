bf6e5c620625e9953cc9993a661c6bd4c61de662
David Mondragon <dragon18saiz@gmail.com>
2023-06-20 09:16:38 -0600

    server.js app -routes midelware, main.handlebars layout

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const sequelize = require('./config/connection');
-
-// model import: 
+const routes = require('./controllers');
+// model import:
 const Users = require('./model');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(routes);
 // sync sequelize models to the database, then turn on the server
-sequelize.sync().then(() => {
+sequelize
+  .sync()
+  .then(() => {
     app.listen(PORT, () => {
       console.log(`App listening on port ${PORT}!`);
     });
-    }).catch((error) => console.log("Error with sequelize link:", error));
+  })
+  .catch((error) => console.log('Error with sequelize link:', error));
