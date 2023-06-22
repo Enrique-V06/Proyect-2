ccade955c1c6cf62b5ac8d476154326d1017c1d4
David Mondragon <dragon18saiz@gmail.com>
2023-06-21 20:36:22 -0600

    corrected user routes on index.js inside api folder in controllers

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const profileRoutes = require('./profileRoutes');
 
 router.use('/profile', userRoutes);
-router.use('profileRoute', profileRoutes);
 
 module.exports = router;
