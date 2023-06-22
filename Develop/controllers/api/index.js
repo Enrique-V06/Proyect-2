bb8f7af43ea52202455001d6cd8d073ce9baa7b8
David Mondragon <dragon18saiz@gmail.com>
2023-06-21 19:56:03 -0600

    index changes

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const profileRoutes = require('./profileRoutes');
 
 router.use('/profile', userRoutes);
 
 module.exports = router;
-
