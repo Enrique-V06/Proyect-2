6f3cba860986b9aad8ca56e2cbff976298f75921
David Mondragon <dragon18saiz@gmail.com>
2023-06-25 14:23:24 -0600

    added stopPropagation in geocodeApi.js to prevent the event propagation when you look for a location

--- Develop/controllers/api/offerRoutes.js
+++ Develop/controllers/api/offerRoutes.js
@@ -27,43 +27,43 @@ const upload = multer({ dest: 'public/images' });
 //   fileFilter: multerFilter,
 // });
 
-// router.post('/upload', upload.single('image'), (req, res) => {
-//   try {
-//     const { name } = req.file;
-//     console.log('IMAGE NAME ON ROUTE', name);
-//     const path = `public/images${name}`;
-//     //console.log('PATH', req.file);
-//     // alert(path);
-//     // const offerData = Offer.create({
-//     //   location: req.body.location,
-//     //   typeOfHome: req.body.typeOfHome,
-//     //   image: path,
-//     //   pet: req.body.pet,
-//     // });
+router.post('/upload', upload.single('image'), (req, res) => {
+  try {
+    console.log('Holiwis');
+    // const { name } = req.image;
+    // console.log('IMAGE NAME ON ROUTE', name);
+    // const path = `public/images${name}`;
+    //console.log('PATH', req.file);
+    // alert(path);
+    // const offerData = Offer.create({
+    //   location: req.body.location,
+    //   typeOfHome: req.body.typeOfHome,
+    //   image: path,
+    //   pet: req.body.pet,
+    // });
 
-//     // Offer.save((err) => {
-//     //   if (err) {
-//     //     console.log('Indeed there was an error', err);
-//     //     return res.status(500).send('Error saving offer');
-//     //   }
-//     //   return res.status(200).send('offer created');
-//     // });
+    // Offer.save((err) => {
+    //   if (err) {
+    //     console.log('Indeed there was an error', err);
+    //     return res.status(500).send('Error saving offer');
+    //   }
+    //   return res.status(200).send('offer created');
+    // });
 
-//     // req.session.save(() => {
-//     //   req.session.loggedIn = true;
-//     //   res.status(200).json(offerData);
-//     //   console.log('response sent');
-//     // });
-//   } catch (err) {
-//     console.log(
-//       err,
-//       'Something happened in the /upload post route, better go check it out!'
-//     );
-//   }
-// });
+    // req.session.save(() => {
+    //   req.session.loggedIn = true;
+    //   res.status(200).json(offerData);
+    //   console.log('response sent');
+    // });
+  } catch (err) {
+    console.log(
+      err,
+      'Something happened in the /upload post route, better go check it out!'
+    );
+  }
+});
 
 //const upload = multer({ dest: 'public/images' });
-
 
 // router.post('/multerTest', upload.single('myFile'), (req, res) => {
 //   console.log('IMAGE STRING', req.file);
