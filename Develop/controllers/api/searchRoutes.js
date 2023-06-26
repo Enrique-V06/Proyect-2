8af637f86938aa08bba0273f4b7dd36f64377264
MariaFernandaMarroquin <MariaFernanda@MacBook-Pro-de-Martha.local>
2023-06-26 11:02:33 -0600

    commit to pull origin main with Dynamically search

--- Develop/controllers/api/searchRoutes.js
+++ Develop/controllers/api/searchRoutes.js
@@ -4,20 +4,23 @@ const { Sequelize } = require('sequelize');
 const { Users, Search, Offer, Review } = require('../../model');
 const router = require('express').Router();
 
-//Make a post
-router.post('/', async (req, res) => {
-    try {
-        const newSearch = await Search.create({
-            location: req.body.loc,
-            typeOfHome: req.body.typeOfHome,
-            pet: req.body.pet
-        });
-        console.log("Search Data", newSearch)
-    } catch (err) {
-        res.status(500).json(err);
-    }
-})
 
+// var newSearch;
+// //Make a post
+// router.post('/', async (req, res) => {
+//     try {
+//         newSearch = await Search.create({
+//             location: req.body.loc,
+//             typeOfHome: req.body.typeOfHome,
+//             pet: req.body.pet
+//         });
+//         res.status(200).json(newSearch)
+//         console.log("Search Data", newSearch)
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// })
+s
 
 // //GET request to display search data
 // //api/search
@@ -42,29 +45,30 @@ router.post('/', async (req, res) => {
 //     }
 // });
 
-router.get('/', async (req, res) => {
-    console.log(req.session)
-    if (!req.session.loggedIn) {
-      res.redirect('/');
-    } else {
-      console.log("loged!")
-      try {
-        const offerData = await Offer.findAll({
-            where: {
-                location: req.body.location,
-                typeOfHome: req.body.typeOfHome,
-                pet: req.body.pet,
-            }
-        }, {include: [{ model: Search }]}
-        );
-        res.render('userhomepage');
 
-      } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-      }
+// router.get('/', async (req, res) => {
+//     console.log(req.session)
+//     if (!req.session.loggedIn) {
+//       res.redirect('/');
+//     } else {
+//       console.log("loged!")
+//       try {
+//         const offerData = await Offer.findAll({
+//             where: {
+//                 location: newSearch.search.dataValues.location,
+//                 typeOfHome: newSearch.search.dataValues.typeOfHome,
+//                 pet: newSearch.search.dataValues.pet,
+//             }
+//         }
+//         );
+//         res.status(200).json(offerData)
+//         console.log("Offer Data", offerData)
+//       } catch (err) {
+//         console.log(err);
+//         res.status(500).json(err);
+//       }
 
-    }
-  });
+//     }
+//   });
 
 module.exports = router;
