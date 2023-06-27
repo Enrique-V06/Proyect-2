1f4903082820cfd5e0b897168556b4846952c90e
MariaFernandaMarroquin <MariaFernanda@MacBook-Pro-de-Martha.local>
2023-06-27 16:12:27 -0600

    last changes

--- Develop/public/js/geocodeApi.js
+++ Develop/public/js/geocodeApi.js
@@ -25,7 +25,7 @@ function getLocationApi(searchAdrr) {
   console.log('   getLocationApi()');
   const queryURL1 = `https://api.opencagedata.com/geocode/v1/json?q=${
     searchAdrr
-  }&key=2f9dc7b017e04d3daa1a27f20c2b9aad`;
+  }&key=${process.env.KEY}`;
 
   fetch(queryURL1)
     .then((response) => response.json())
